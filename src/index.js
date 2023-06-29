d720bb2dd1787b9086b3d8de6fb9e7ac1dafeff4
Karol <kpulawski13@gmail.com>
2023-06-29 17:03:56 +0100

    wired up all modules

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import renderContactPage from "./contact.js";
 const content = document.getElementById("content");
 const btns = document.querySelectorAll("button");
 btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const page = e.target.innerText;
-    console.log(page);
-    content.textContent = "";
-    if (page === "Home") renderHomePage();
-    if (page === "Menu") renderMenuPage();
-    if (page === "Contact") renderContactPage();
-  });
+  btn.addEventListener("click", renderPageAndMarkButton);
 });
+
+function renderPageAndMarkButton(e) {
+  btns.forEach((btn) => {
+    btn.classList.remove("clicked");
+  });
+  this.classList.add("clicked");
+  const page = e.target.innerText;
+  console.log(page);
+  content.textContent = "";
+  this.add;
+  if (page === "Home") renderHomePage();
+  if (page === "Menu") renderMenuPage();
+  if (page === "Contact") renderContactPage();
+}
+
 renderHomePage();
-g;
